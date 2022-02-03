1cc46b990a6bf79177fb8ff2e1a7be1c7357819c
Doris Morales <moralesdoris880@gmail.com>
2022-02-02 19:01:20 -0500

    Completed assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,14 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words=[]
+
+    words.each do |word|
+        reversed_words << word.reverse
+      end
+      reversed_words.join(" ")
+  end
+
+reverse_each_word("Hello there, and how are you?")
